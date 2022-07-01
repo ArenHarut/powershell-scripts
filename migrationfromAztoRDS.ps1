27439e5f5161d37aeb46ff35300656b7284c9eb6
ArenHarut <harutyunyanaren@mail.ru>
2022-07-01 13:42:59 +0400

    migration script 2

--- migrationfromAztoRDS.ps1
+++ migrationfromAztoRDS.ps1
@@ -9,14 +9,18 @@ $sourceserver = Read-Host "Enter Azure Source SQL server. Means name without .da
 $dbname = Read-Host "Enter Azure SQL DB name need to be copied"
 $targetserver = Read-Host "Enter Azure SQL Server name where you want to restore the copy. Means name without .databas.windows.net"
 $copyname = Read-Host "Enter Azure SQL copy DB name"
-$epool = Read-Host "Enter Azure SQL Epool name which needs to be created before this script runs"
-$azuresvusername = Read-Host "Enter Azure SQL server admin username" -MaskInput
-$azuresvpassword = Read-Host "Enter Azure SQL admin servier password" -AsSecureString
+$epoolname = Read-Host "Enter Azure SQL Epool name which needs to be created before this script runs"
+$azuresvusername = Read-Host "Enter Azure SQL Server admin username"
+$azuresvpassword = Read-Host "Enter Azure SQL Server password" -AsSecureString
+$azuresvpassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($azuresvpassword))
 $sqlsvname = Read-Host "Enter Azure SQL Server full dns name"
 $awsrdsserver = Read-Host "Enter AWS RDS server dns name"
 $awsrdsdb = Read-Host "Enter AWS RDS db name"
 $awsrdsusername = Read-Host "Enter AWS RDS username" -MaskInput
 $awsrdspass = Read-Host "Enter AWS RDS server password" -AsSecureString
+$awsrdspass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($awsrdspass))
+$azuresqlsvfqdn = "brit-rb-production-sql-01.database.windows.net"
+$pathtofile = "D:\Users\I29655\Documents\Brit-Rulebook-2-4-Prod-Primary.bacpac"
 
 echo "Installing dependencies"
 
@@ -31,11 +35,22 @@ Install-Module Az
 Import-Module Az.Accounts
 Connect-AzAccount -Subscription $subid
 
+echo "Checking if Epool exists. If not create one"
+
+$epool = Get-AzSqlElasticPool -ResourceGroupName $rgname -ServerName $sourceserver -ElasticPoolName $epoolname -ErrorAction SilentlyContinue
+
+if($epool -eq $null){
+    $epoolcreate =  New-AzSqlElasticPool -ResourceGroupName $rgname -ServerName $sourceserver -ElasticPoolName $epoolname -Edition "Standard" -Dtu 800
+}
+else{
+    Write-Host "$epoolname already exist"
+}
+
 echo "Copying Azure original DB"
 
 
 New-AzSqlDatabaseCopy -ResourceGroupName $rgname -ServerName $sourceserver -DatabaseName $dbname `
-    -CopyResourceGroupName $rgname -ElasticPoolName $epool -CopyServerName $targetserver -CopyDatabaseName $copyname
+    -CopyResourceGroupName $rgname -ElasticPoolName $epoolname -CopyServerName $targetserver -CopyDatabaseName $copyname
 
 
 Install-Module -Name SQLServer
@@ -46,22 +61,17 @@ echo "Deleting Custom users in copied DB"
 #You should input the SQL script within query quotes before executing the scripts. Just replace usernames that you see in closed brackets with actual usernames
 #that you notice in Original DB
 
-Invoke-Sqlcmd -ServerInstance "brit-rb-production-sql-01.database.windows.net" -Database $copyname -Username $azuresvusername -Password $azuresvpassword `
--query 'Drop user [brit-rb-prod-author]
-Drop user [brit-rb-prod-coreserver]
-Drop user [brit-rb-prod-dataservices]
-Drop user [brit-rb-prod-i2quotewebapp]
-Drop user [brit-rb-prod-maintenancewebapp]
-Drop user [brit-rb-prod-quotewebapp]'
+Invoke-Sqlcmd -ServerInstance $azuresqlsvfqdn -Database $copyname -Username $azuresvusername -Password $azuresvpassword `
+-query Read-Host "Delete users script"
 
-echo "Starting export of a DB"
+echo "Starting export of a DB. To do this you should have sqlpackage.exe installed. You can get it from here: https://docs.microsoft.com/en-us/sql/tools/sqlpackage/sqlpackage-download?view=sql-server-ver16"
 
 cd 'C:\Program Files\Microsoft SQL Server\150\DAC\bin\'
 
-.\sqlpackage.exe /a:Export /tf:"D:\Users\I29655\Documents\Brit-Rulebook-2-4-Prod-Primary.bacpac" /ssn:$sqlsvname /sdn:$copyname /su:$azuresvusername /sp:$azuresvpassword
+.\sqlpackage.exe /a:Export /tf:$pathtofile /ssn:$sqlsvname /sdn:$copyname /su:$azuresvusername /sp:$azuresvpassword
 
 
 echo "Importing bacpac of the DB to AWS"
 
 
-.\sqlpackage.exe /a:Import /sf:"D:\Users\I29655\Documents\Brit-Rulebook-2-4-Prod-Primary.bacpac" /tsn:$awsrdsserver /tdn:$awsrdsdb /tu:$awsrdsusername /tp:$awsrdspass
+.\sqlpackage.exe /a:Import /sf:$pathtofile /tsn:$awsrdsserver /tdn:$awsrdsdb /tu:$awsrdsusername /tp:$awsrdspass
